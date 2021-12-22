c47a8d6ded17dee071f73ffabacfe1280b6b05ac
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-12-22 11:09:23 -0300

    Paginação estilizada com styled-components

--- countries/src/Paginate/Paginate.js
+++ countries/src/Paginate/Paginate.js
@@ -1,12 +1,14 @@
 import { pagix } from 'pagix';
+import {Button, Div} from './style.js'
+
 
 const Page = ({ idx, active, onChange }) => {
     const handleClick = () => onChange(idx)
 
     return (
-        <button onClick={handleClick}>
+        <Button onClick={handleClick} style= {{background: active ? "#6D2080" : "#FFFFFF", color: active ? "white" : "#8D8D8D"}} >
             {idx}
-        </button>
+        </Button>
     )
 }
 
@@ -20,34 +22,34 @@ export const Paginate = ({ records, limit, current, delta, fixed, onChange }) =>
         onChange 
     })
 
-    console.log({ total, start, end, middle, prev, next, from, to })
-
+    console.log('start', start)
+    console.log('end', end)
+    
     const goToPrev = () => onChange(prev)
     const goToNext = () => onChange(next)
 
     return (
-        <> 
-            <div flex={1}>
-                <text>
-                    Showing <strong>{from}</strong>-<strong>{to}</strong> of {records}
-                </text>
-            </div>
+        <Div> 
             
+            {start && start[0] !== current && (<Button onClick= {() => onChange(current - 1)}> {`<`} </Button>)}
+
             {start.map(idx => (
                 <Page idx={idx} active= {current === idx} onChange= {onChange} />
             ))}
 
-            {prev && (<button onClick= {goToPrev}> ... </button>)}
+            {prev && (<Button onClick= {goToPrev}> ... </Button>)}
 
             {middle.map(idx => (
                 <Page idx={idx} active= {current === idx} onChange= {onChange} />
             ))}
 
-            {next && (<button onClick= {goToNext} > ... </button>)}
+            {next && (<Button onClick= {goToNext} > ... </Button>)}
 
             {end.map(idx => (
                 <Page idx={idx} active= {current === idx} onChange= {onChange} />
             ))}
-        </>
+
+            {end && end[0] !== current && (<Button onClick= {() => onChange(current + 1)} > {`>`} </Button>)}
+        </Div>
     )
 }
---
+++ countries/src/Paginate/style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    
+    background: #FFFFFF;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.25);
+    border-radius: 2px;
+    border: 0px;
+
+    width: 34px;
+    height: 34px;
+
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 20px;
+    color: #8D8D8D;
+
+    margin: 10px;
+    cursor: pointer;
+`
+
+export const Div = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+`
